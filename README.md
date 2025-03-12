e689b7eff1ca8f43a0c427b04eee751abba4b15f
taisfonsec <tais.fonsec3@gmail.com>
2025-03-11 22:07:28 -0300

    Calculadora

---
+++ README.md
@@ -0,0 +1,3 @@
+# Calculadora Básica
+
+Este projeto é uma calculadora básica desenvolvida com HTML, CSS e JavaScript, que realiza operações matemáticas simples como adição, subtração, multiplicação e divisão. A calculadora possui uma interface intuitiva, com botões que permitem inserir números e operadores no display, além de funcionalidades para limpar todos os dados, apagar o último dígito e exibir o resultado das operações de forma dinâmica. O objetivo deste projeto é demonstrar habilidades em desenvolvimento web, lógica de programação e criação de interfaces interativas.
---
+++ script.js
@@ -0,0 +1,44 @@
+// Aparecer digito no display
+function inserirDisplay(digito) {
+    const display = document.querySelector('#display')
+    const ultimoCaracter = display.value.slice(-1)
+
+    // Impede de começar com um operador inválido
+    if (display.value === '' && ['+', '-', '*', '/'].includes(digito)) {
+        return
+    }
+
+    // Impede dois operadores seguidos
+    if (['+', '-', '*', '/'].includes(ultimoCaracter) && ['+', '-', '*', '/'].includes(digito)) {
+        return
+    }
+    
+    display.value += digito
+}
+
+// Realizar cálculo
+function calcular(){
+    const display = document.querySelector('#display')
+
+    // Impede o calcular com valor vazio
+    if (display.value == '') {
+        display.value = '0'
+    } else {
+        try {
+            display.value = new Function("return " + display.value)()
+        } catch (error) {
+            display.value = 'ERRO'
+        }
+    }
+}
+
+// Limpar todos os digitos
+function limparTudo() {
+    document.querySelector('#display').value = ''
+}
+
+// Apagar apenas um digito
+function apagar() {
+    const display = document.querySelector('#display')
+    display.value = display.value.slice(0, -1)
+}
